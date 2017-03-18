44ec3c52099211a719daf2fdcf723df8dd8ded06
asamanta <ajoy2008.cse@gmail.com>
2017-03-18 15:22:34 +0530

    Added sample project for multiple file compilation

---
+++ 02_multi_file_compilation/main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include"myinclude.h"
+int main(void)
+{
+  printf("\nadd(20, 10) = %d\n", add(20,10));
+  printf("\nsub(20, 10) = %d\n", sub(20,10));
+  return 0;
+}
---
+++ 02_multi_file_compilation/myinclude.h
@@ -0,0 +1,7 @@
+#ifndef __MYINCLUDE_H__
+#define __MYINCLUDE_H__ 1
+
+int add(int a, int b);
+int sub(int a, int b);
+
+#endif
